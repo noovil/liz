25ee4892a126de1ba89abb23dbd2da0cdb5b7a5f
noovil <lokelivoon@gmail.com>
2022-03-27 11:08:25 +0800

    initial commit

---
+++ js/background.js
@@ -0,0 +1,18 @@
+function handleClick(id) {
+    //console.log("clicked!")
+    if (id === "insta") {
+        window.open("https://www.instagram.com/barryzeunicorn/");
+    }
+
+    else if (id === "twitter") {
+        window.open("https://twitter.com/barryzeunicorn");
+    }
+
+    else if (id === "yt") {
+        window.open("https://www.youtube.com/channel/UCvZBk6_ost4KS5xMDf21iCw");
+    }
+
+    else if (id === "email") {
+        window.open("mailto:lokelivoon@gmail.com");
+    }
+}
